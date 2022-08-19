0981c34be78d681c7bbaaf3c1f13f6364f1534aa
arshi016 <arshilife16@gmail.com>
2022-08-19 02:51:48 -0400

    Add files via upload

--- caesar.py
+++ caesar.py
@@ -9,15 +9,24 @@ def encrypt_casear(plaint_text, key):
 
 def decrypt_caesar(cipher_text, key):
     plaint_text = ''
-    for i in plaint_text:
+    for i in cipher_text:
         index = (alphabets.find(i) - key) % len(alphabets)
         plaint_text = plaint_text + alphabets[index]
     return plaint_text
 
+def crack_caesar(cipher_text):
+    for key in range(len(alphabets)):
+        plaint_text = ''
+        for i in cipher_text:
+            index = (alphabets.find(i) - key) % len(alphabets)
+            plaint_text = plaint_text + alphabets[index]
+        print("Key = ", key , "    Decrypted msg = " , plaint_text)
+        
 def main():
-    test = "abcde"
+    test = "i am so good"
     test = test.lower()
     print(encrypt_casear(test, 3))
+    print(crack_caesar("lcdpcvrcjrrg"))
 
 if __name__ == '__main__':
     main()
