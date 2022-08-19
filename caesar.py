262a68a04aa7dc6d9d433ef1526671892dc2222e
arshi016 <arshilife16@gmail.com>
2022-08-19 02:39:27 -0400

    Add files via upload

---
+++ caesar.py
@@ -0,0 +1,22 @@
+alphabets = " abcdefghijklmnopqrstuvwxyz"
+
+def encrypt_casear(plaint_text, key):
+    cipher_text = ''
+    for i in plaint_text:
+        index = (alphabets.find(i) + key) % len(alphabets)
+        cipher_text = cipher_text + alphabets[index]
+    return cipher_text
+
+def decrypt_caesar(cipher_text, key):
+    plaint_text = ''
+    for i in plaint_text:
+        index = (alphabets.find(i) - key) % len(alphabets)
+        plaint_text = plaint_text + alphabets[index]
+    return plaint_text
+
+def main():
+    test = "abcde"
+    print(encrypt_casear(test, 3))
+
+if __name__ == '__main__':
+    main()
